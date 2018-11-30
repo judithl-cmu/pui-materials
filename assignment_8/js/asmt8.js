db88b16fc36ce3e5d3fab50e0621301c69ad48a3
Judy <judithl@andrew.cmu.edu>
2018-11-30 14:11:27 -0500

    11/30 1st commit

--- assignment_8/js/asmt8.js
+++ assignment_8/js/asmt8.js
@@ -3,13 +3,21 @@ $(document).ready(function () {
 
     // correspond animation with scroll position
 
-    // var startPos = $("#timeline").offset();
-    // var scrollPos = $("#timeline").scrollTop - startPos;
-    // var svgLen = $("#timeline").height();
-    //
-    // $("#timeline-path").style.strokeDashoffset = svgLen - scrollPos + startPos;
+    // image blur and fade
+    var viewportHt = $(window).height();
 
-    // hover to show info about other artists
+    $(window).on("scroll", function(){
+
+        // var scrollPos = $(window).scrollTop();
+        // $(".img-blur").css({
+        //     filter: "blur(" + scrollPos + ")px"
+        // });
+        $(".img-fade").css(
+            "opacity", 1 - $(window).scrollTop() / viewportHt
+        );
+    });
+
+    // click to show info about other artists
 
     var artistName = "";
     var artistContent = "";
@@ -18,75 +26,19 @@ $(document).ready(function () {
     // check if class name exists
     $("span").on("click", function(){
 
-        // check if span has been clicked
+        // check if class exists
 
-        // if (isClicked === false) {
-        //     artistName = $(this).text();
-        //     console.log(artistName);
-        //     $("#artist-name").text(artistName);
-        //     //switch statement to determine which artist content and link to attach
-        //     switch (artistName) {
-        //         case "Gustave Courbet":
-        //             artistContent = "Jean Désiré Gustave Courbet was a French painter who led the Realism movement in 19th-century French painting.";
-        //             artistLink = "https://en.wikipedia.org/wiki/Gustave_Courbet";
-        //             break;
-        //         case "Edouard Manet":
-        //             artistContent = "Édouard Manet was one of the first 19th-century artists to paint modern life, and a pivotal figure in the transition from Realism to Impressionism.";
-        //             artistLink = "https://en.wikipedia.org/wiki/%C3%89douard_Manet";
-        //             break;
-        //         case "Charles Gleyre":
-        //             artistContent = "Marc Gabriel Charles Gleyre was a Swiss artist who was a resident in France from an early age. He taught a number of younger artists who became prominent.";
-        //             artistLink = "https://en.wikipedia.org/wiki/Charles_Gleyre";
-        //             break;
-        //         case "Pierre-Auguste Renoir":
-        //             artistContent = "As a celebrator of beauty and especially feminine sensuality, it has been said that \"Renoir is the final representative of a tradition which runs directly from Rubens to Watteau.\"";
-        //             artistLink = "https://en.wikipedia.org/wiki/Pierre-Auguste_Renoir";
-        //             break;
-        //         case "Frédéric Bazille":
-        //             artistContent = "Frédéric Bazille was a French Impressionist painter. Many of Bazille's major works are examples of figure painting in which he placed the subject figure within a landscape painted en plein air.";
-        //             artistLink = "https://en.wikipedia.org/wiki/Fr%C3%A9d%C3%A9ric_Bazille";
-        //             break;
-        //         case "Alfred Sisley":
-        //             artistContent = "Alfred Sisley was the most consistent of the Impressionists in his dedication to painting landscape en plein air; he found that Impressionism fulfilled his artistic needs.";
-        //             artistLink = "https://en.wikipedia.org/wiki/Alfred_Sisley";
-        //             break;
-        //         case "Sisley":
-        //             artistContent = "Alfred Sisley was the most consistent of the Impressionists in his dedication to painting landscape en plein air; he found that Impressionism fulfilled his artistic needs.";
-        //             artistLink = "https://en.wikipedia.org/wiki/Alfred_Sisley";
-        //             break;
-        //         case "Paul Cézanne":
-        //             artistContent = " was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century.";
-        //             artistLink = "https://en.wikipedia.org/wiki/Paul_C%C3%A9zanne";
-        //             break;
-        //         case "Camille Pissarro":
-        //             artistContent = "Camille Pissarro was a Danish-French Impressionist and Neo-Impressionist painter.";
-        //             artistLink = "https://en.wikipedia.org/wiki/Camille_Pissarro";
-        //             break;
-        //         case "Edgar Degas":
-        //             artistContent = "Edgar Degas is especially identified with the subject of dance; more than half of his works depict dancers. Regarded as one of the founders of Impressionism, he rejected the term, preferring to be called a realist.";
-        //             artistLink = "https://en.wikipedia.org/wiki/Edgar_Degas";
-        //             break;
-        //     }
-        //     $("#artist-intro").text(artistContent);
-        //     $(".link").attr("href", artistLink);
-        //     var divHeight = $("#other-artists").height();
-        //     var divWidth = $("#other-artists").outerWidth();
-        //     this.append($("#other-artists")[0]);
-        //
-        // }
-        // $("#other-artists").css({
-        //     "top": -divHeight - 60,
-        //     "left": - 100,
-        //     "display": "block",
-        // });
+        if ($(".clickedInfo")[0]) {
+            $("[class*='clickedInfo']").removeClass("clickedInfo");
+        }
+
+        // clear content before each append event
         if ($(this).children().length > 0) {
             $("#artist-name").text("");
             $("#artist-intro").text("");
             $("#link").text("");
             $("#link").attr("href", "");
         }
-
-        console.log($(this).children()[0]);
 
             artistName = $(this).text();
             $("#artist-name").text(artistName);
@@ -120,8 +72,8 @@ $(document).ready(function () {
                     artistContent = "Alfred Sisley was the most consistent of the Impressionists in his dedication to painting landscape en plein air; he found that Impressionism fulfilled his artistic needs.";
                     artistLink = "https://en.wikipedia.org/wiki/Alfred_Sisley";
                     break;
-                case "Paul Cézanne":
-                    artistContent = " was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century.";
+                case "Paul Cezanne":
+                    artistContent = "Paul Cezanne was a French artist and Post-Impressionist painter whose work laid the foundations of the transition from the 19th-century conception of artistic endeavor to a new and radically different world of art in the 20th century.";
                     artistLink = "https://en.wikipedia.org/wiki/Paul_C%C3%A9zanne";
                     break;
                 case "Camille Pissarro":
@@ -138,23 +90,26 @@ $(document).ready(function () {
             $("#link").attr("href", artistLink);
             var divHeight = $("#other-artists").height();
             this.append($("#other-artists")[0]);
-            console.log("successfully appended!");
 
-            // console.log($("#other-artists").parent());
             $("#other-artists").css({
-                "top": -divHeight - 60,
+                "top": -divHeight - 45,
                 "left": - 100,
                 "display": "block",
+                "font-weight": 500
             });
+
+            $(this).addClass("clickedInfo");
     });
 
-    // manipulate position of 1874
-    var impre1Pos = $("#impression-1").offset().top;
-    var impre1Height = $("#impression-1").height();
+    // close popup if clicked outside
 
-    // i have this question
-    $("#impression-2").css({
-        "top": impre1Pos + impre1Height + 30
+    $(document).on("click", function() {
+        var clickTarget = $(event.target);
+        if (!clickTarget.hasClass("clickedInfo")) {
+            $("#other-artists").css({
+                "display": "none"
+            });
+        }
     });
 
     // monet's paintings as particles
@@ -240,11 +195,49 @@ $(document).ready(function () {
         winsize();
     });
 
+    function isInView(x) {
+        var xTop = x.offset().top;
+        var xBottom = xTop + x.outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+
+        return xBottom > viewportTop && xTop < viewportBottom;
+    }
+
+    var winHt = $(window).height();
+    var winWth = $(window).width();
+    function ripplesInit() {
+        $("#waterlilies-painting").ripples({
+            resolution: 512,
+            dropRadius: 30 * Math.random(),
+            perturbance: 0.06
+        });
+        for (var j = 0; j <= 20; j++) {
+            $("#waterlilies-painting").ripples("drop", winHt * Math.random(), winWth * Math.random(), 24 * Math.random(), 0.01 + 0.03 * Math.random());
+        }
+    }
+
+
     $(function(){
         $(w).on('load',function(){
             winsize();
-            // temporarily disable
-            // dotsInit();
+
+            $(w).on("resize scroll", function() {
+                var dotsInView = isInView($(".container-giverny"));
+                var ripplesInView = isInView($("#waterlilies-painting"));
+
+                // if dots container is in view, generate dots
+                if (dotsInView) {
+                    dotsInit();
+                }
+
+                // if waterlilies are in view, generate ripples
+                else if (ripplesInView) {
+                    ripplesInit();
+                }
+            });
+
+
         });
 
     });
@@ -254,5 +247,9 @@ $(document).ready(function () {
             radius: 400
         });
     });
+
+
+
+
 
 });
